4a7bbda43724af942fa349f756c4805d0262926a
ashhadahsan <63206988+ashhadahsan@users.noreply.github.com>
2020-11-30 23:24:32 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -143,7 +143,7 @@ mydb=DATABASE()
 #chrome_options.add_argument('log-level=3')
 # driver=webdriver.Chrome('./driver/chromedriver.exe',options=chrome_options)
 firefox=Options()
-firefox.headless=True
+firefox.headless=False
 firefox.add_argument('log-level=3')
 
 
